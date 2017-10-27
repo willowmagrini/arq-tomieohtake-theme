96ebb03affcc7a5a6004ca34a84e97807e86b953
Diego Rojas <diego@brasa.art.br>
2017-10-27 08:19:46 -0200

    primeira versão do tema com visual do site atual

---
+++ footer.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package coletivo
+ */
+?>
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        <div class="footer-logos">
+            <div class="container">
+            <hr>
+                <div class="row logos">
+                    <div class="col-md-2">
+                        <h3>&nbsp;<br>&nbsp;</h3>
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_paitoan_logo_A.png" style="width: 160px;">
+                    </div>
+                    <div class="col-md-2">
+                        <h3>PATROCÍNIO<br>&nbsp;</h3>
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_paitoan_logo_B.png" style="width: 160px;">
+                    </div>
+                    <div class="col-md-2">
+                        <h3>&nbsp;<br>&nbsp;</h3>
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_paitoan_logo_B2.png" style="width: 160px;">
+                    </div>
+
+                    <div class="col-md-2">
+                        <h3>APOIO DE MÍDIA<br>&nbsp;</h3>
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_paitoan_logo_C2.png" style="width: 80px;">
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_paitoan_logo_C3.png" style="width: 80px;">
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_paitoan_logo_C4.png" style="width: 80px;">
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_paitoan_logo_C5.png" style="width: 80px;">
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_paitoan_logo_C6.png" style="width: 80px;">
+                    </div>
+                    <div class="col-md-2">
+                        <h3>IDEALIZAÇÃO E<br> COORDENAÇÃO GERAL</h3>
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_paitoan_logo_D.png" style="width: 160px;">
+                    </div>
+                    <div class="col-md-2">
+                        <h3>REALIZAÇÃO<br>&nbsp;</h3>
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_paitoan_logo_E.png" style="width: 160px;">
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_logo_menu.png" style="height: 50px;">
+                        <h4 style="margin-left:69px;">
+                            Av.Brigadeiro Faria Lima, 201<br>
+                            Pinheiros - São Paulo - SP<br>
+                            CEP 05426-100
+                        </h4>
+                    </div>
+                    <div class="col-md-2 col-lg-offset-4">
+                        <img src="<?php echo get_stylesheet_directory(); ?>/images/ito_index_logo2.png" class="img-responsive">
+                    </div>
+                </div>
+                <hr>
+            </div>
+        </div>
+
+        <?php $coletivo_btt_disable = get_theme_mod('coletivo_btt_disable'); ?>
+        <div class="site-info">
+            <div class="container">
+                <?php
+                /**
+                 * hooked coletivo_footer_site_info
+                 * @see coletivo_footer_site_info
+                 */
+                do_action('coletivo_footer_site_info');
+                ?>
+            </div>
+        </div>
+        <!-- .site-info -->
+    </footer><!-- #colophon -->
+<?php
+/**
+ * Hooked: coletivo_site_footer
+ *
+ * @see coletivo_site_footer
+ */
+do_action( 'coletivo_site_end' );
+?>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- functions.php
+++ functions.php
@@ -6,24 +6,24 @@
 
 // Altera a função do footer, originalmente no arquivo inc/template-tags.php
 
-if ( ! function_exists( 'onepress_footer_site_info' ) ) {
+if ( ! function_exists( 'coletivo_footer_site_info' ) ) {
     /**
      * Add Copyright and Credit text to footer
      * @since 1.1.3
      */
-    function onepress_footer_site_info()
+    function coletivo_footer_site_info()
     {
         ?>
-        <?php $onepress_footer_text = get_theme_mod( 'onepress_footer_text', esc_html__('Few Rights Reserved', 'onepress') );?>
-        <?php $onepress_footer_text_link = get_theme_mod( 'onepress_footer_text_link' );?>
-        <?php if ( $onepress_footer_text_link != '' ) echo '<a href="' . esc_html( $onepress_footer_text_link) . '" alt="" target="_blank">'; ?>
-        <?php if ( $onepress_footer_text != '' ) echo '<div class="container">' . esc_html( $onepress_footer_text) . '</div>'; ?>
-        <?php if ( $onepress_footer_text_link != '' ) echo '</a>'; ?>
+        <?php $coletivo_footer_text = get_theme_mod( 'coletivo_footer_text', esc_html__('Few Rights Reserved', 'coletivo') );?>
+        <?php $coletivo_footer_text_link = get_theme_mod( 'coletivo_footer_text_link' );?>
+        <?php if ( $coletivo_footer_text_link != '' ) echo '<a href="' . esc_html( $coletivo_footer_text_link) . '" alt="" target="_blank">'; ?>
+        <?php if ( $coletivo_footer_text != '' ) echo '<div class="container">' . esc_html( $coletivo_footer_text) . '</div>'; ?>
+        <?php if ( $coletivo_footer_text_link != '' ) echo '</a>'; ?>
 
-        <?php printf(esc_html__('%2$s %1$s', 'onepress'), esc_attr(date('Y')), esc_attr(get_bloginfo())); ?>
+        <?php printf(esc_html__('%2$s %1$s', 'coletivo'), esc_attr(date('Y')), esc_attr(get_bloginfo())); ?>
         <span class="sep"> &ndash; </span>
-        <?php printf(esc_html__('Desenvolvido pela %1$s com %2$s', 'onepress'), '<a class="logo-brasa" href="' . esc_url('http://brasa.art.br', 'onepress') . '">Brasa</a>', '<a class="logo-wp" href="' . esc_url('https://br.wordpress.org', 'onepress') . '"><i class="fa fa-wordpress" aria-hidden="true"></i></a>'); ?>
+        <?php printf(esc_html__('Desenvolvido pela %1$s com %2$s', 'coletivo'), '<a class="logo-brasa" href="' . esc_url('http://brasa.art.br', 'coletivo') . '">Brasa</a>', '<a class="logo-wp" href="' . esc_url('https://br.wordpress.org', 'coletivo') . '"><i class="fa fa-wordpress" aria-hidden="true"></i></a>'); ?>
         <?php
     }
 }
-add_action( 'onepress_footer_site_info', 'onepress_footer_site_info' );
+add_action( 'coletivo_footer_site_info', 'coletivo_footer_site_info' );
