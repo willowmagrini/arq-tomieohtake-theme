9082aad06457c3512f440f94b884f445eef0eb2c
Diego Rojas <diego@brasa.art.br>
2017-11-07 22:09:49 -0200

    revisão de email de confirmação

--- inc/email-user.php
+++ inc/email-user.php
@@ -4,7 +4,7 @@
 <p>Olá,
 agradecemos sua inscrição na Residência Artística!</p>
 
-<p>Até o dia 22 de dezembro de 2017 será possível a edição de seu cadastro, bem como excluir os arquivos já enviados e substituí-los por novos arquivos. Mas lembre-se de salvar as alterações realizadas no sistema.</p>
+<p>Até o dia 22 de dezembro de 2017 será possível a edição de seu cadastro, bem como excluir os arquivos já enviados e substituí-los por novos arquivos. Mas lembre-se de atualizar as alterações realizadas no sistema.</p>
 
 <p>Dúvidas podem ser enviadas ao email: premioedpnasartes@institutotomieohtake.org.br</p>
 
