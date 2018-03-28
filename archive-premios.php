dfd1341797430639a55f606989073226e9aeab86
Diego Rojas <diego@brasa.art.br>
2018-03-28 10:19:57 -0300

    resolvida archive de premios

--- archive-premios.php
+++ archive-premios.php
@@ -15,11 +15,40 @@ get_header(); ?>
 					</h2>
 					<br />
 							    
-				    <?php while ( have_posts() ) : the_post(); ?>
+				    
+						<?php
+							/* $paged é a variável para paginação do Loop CPT Prêmios */	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
-				        <?php get_template_part( 'content', 'list' ); ?>
- 
-       				<?php endwhile; // end of the loop. ?>
+							/* $args_loop_cpt_prêmios são os argumentos para o Loop */
+							$args_loop_cpt_premios = array(
+							'post_type' => 'premios',
+							'orderby' => 'date',
+							'order' => 'DESC',
+							'posts_per_page' => '4',
+							'paged' => $paged
+							);
+							$loop_cpt_premios = new WP_Query( $args_loop_cpt_premios ); if ( $loop_cpt_premios->have_posts() ) {
+							while ( $loop_cpt_premios->have_posts() ) : $loop_cpt_premios->the_post();
+						?>
+							
+						<div class="list-article">
+
+							<div class="list-article-thumb">
+								<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('list-article-thumb'); ?></a>
+							</div><!-- .list-article-thumb -->
+						
+							<div class="rodape-cada-projeto">
+								<h3><a class="titulo-resumo" href="<?php the_permalink(); ?>"><?php the_title(); ?><br />						
+								<!-- <span class="data-cada-post">METADADO</span> -->
+							</div><!-- .rodape-cada-projeto -->
+						</div><!-- .list-article -->
+
+						<?php
+							// Fim do Loop
+							endwhile;
+						}
+						?>
+
 
 			</main><!-- #main -->
 		</div><!-- #primary -->
